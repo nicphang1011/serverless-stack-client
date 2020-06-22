44d535b84e4c0ba03db5dc77348af56896e65090
NicPhang <33155076+NicPhang@users.noreply.github.com>
2020-06-22 11:50:59 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "nicnotesappuploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://rhkutvii60.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_DkUtSwjmU",
+    APP_CLIENT_ID: "49r6vuu9qkp7r2ia1q28kt7oeb",
+    IDENTITY_POOL_ID: "us-east-1:f059bdbd-2c86-4a7b-84c8-a88cf64054be"
+  }
+};
